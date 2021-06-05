e5f13f447c7e2e7c4d06c8afa44c9d48d36e0b22
Luka Steinbach <luka.steinbach@gmx.de>
2021-06-05 13:33:28 +0200

    Added logging to run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -9,17 +9,31 @@ mkdir -p $SCRIPT_PATH/../module/src/
 mkdir -p $SCRIPT_PATH/../module/lib/
 mkdir -p $SCRIPT_PATH/../module/instrumented/
 # npm --prefix $SCRIPT_PATH/../module/src install --only=dev $MODULE_NAME
+
+echo ""
+echo ">> Cloning module..."
 git clone $MODULE_URL module/src
 
+echo ""
+echo ">> Compiling module with Babel..."
 npm run build
+
+echo ""
+echo ">> Instrumenting module with Jalangi..."
 npm run instrument
 
+echo ""
+echo ">> Installing module dependencies..."
 cd $ROOT_PATH/module/instrumented
 npm install
 cd $ROOT_PATH
 
+echo ""
+echo ">> Parsing test script..."
 node tools/testScript.js $ROOT_PATH/module/instrumented/package.json
 
+echo ""
+echo ">> Running tests..."
 cd $ROOT_PATH/module/instrumented
 npm run __test__
 cd $ROOT_PATH
