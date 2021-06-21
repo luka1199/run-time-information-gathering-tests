d8c78e4387fb0dfd66e33fce092dea36b15bc95c
Luka Steinbach <luka.steinbach@gmx.de>
2021-06-21 17:10:49 +0200

    Completed instrument_all

--- bin/instrument_all_babel.sh
+++ bin/instrument_all_babel.sh
@@ -9,15 +9,25 @@ rm -f $LOG_FILE
 
 for MODULE in $MODULES_FOLDER/*;
 do  
-    npx babel -D $MODULE/src -d $MODULE/$TARGET_DIR_NAME
-    # echo "$BABEL_OUTPUT"
-    echo "Instrumenting module '$(basename "$MODULE")' to $MODULE/$OUTPUT_DIR_NAME"
-    INSTRUMENT_OUTPUT=$($SCRIPT_PATH/instrument.sh "$MODULE/$TARGET_DIR_NAME" "$MODULE/$OUTPUT_DIR_NAME")
-    if [[ ! "$INSTRUMENT_OUTPUT" == *"Failed to instrument"* ]];
-    then 
-        LOG_OUTPUT="OK"
-    else 
+    echo ""
+    rm -rf $MODULE/$TARGET_DIR_NAME
+    rm -rf $MODULE/$OUTPUT_DIR_NAME
+    
+    echo "Compiling module '$(basename "$MODULE")' to $MODULE/$TARGET_DIR_NAME"
+    BABEL_ERROR=$(npx babel -D $MODULE/src -d $MODULE/$TARGET_DIR_NAME 2>&1 >/dev/null)
+    if [[ $BABEL_ERROR != "" ]];
+    then
+        echo "ERROR"
         LOG_OUTPUT="NOK"
+    else
+        echo "Instrumenting module '$(basename "$MODULE")' to $MODULE/$OUTPUT_DIR_NAME"
+        INSTRUMENT_OUTPUT=$($SCRIPT_PATH/instrument.sh "$MODULE/$TARGET_DIR_NAME" "$MODULE/$OUTPUT_DIR_NAME")
+        if [[ ! "$INSTRUMENT_OUTPUT" == *"Failed to instrument"* ]];
+        then 
+            LOG_OUTPUT="OK"
+        else 
+            LOG_OUTPUT="NOK"
+        fi
     fi
     echo "$(basename "$MODULE") - $LOG_OUTPUT" >> $LOG_FILE
 done
