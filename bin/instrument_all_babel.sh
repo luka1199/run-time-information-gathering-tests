06ef1a71e3b2497c0b02454a07f528dd6ddc6f96
Luka Steinbach <luka.steinbach@gmx.de>
2021-06-21 16:52:04 +0200

    Readded instrument_all_babel.sh

--- bin/instrument_all_babel.sh
+++ bin/instrument_all_babel.sh
@@ -9,15 +9,15 @@ rm -f $LOG_FILE
 
 for MODULE in $MODULES_FOLDER/*;
 do  
-    # $SCRIPT_PATH/../node_modules/.bin/babel -D $MODULE/src -d $MODULE/$TARGET_DIR_NAME
+    npx babel -D $MODULE/src -d $MODULE/$TARGET_DIR_NAME
     # echo "$BABEL_OUTPUT"
-    # echo "Instrumenting module '$(basename "$MODULE")' to $MODULE/$OUTPUT_DIR_NAME"
-    # INSTRUMENT_OUTPUT=$($SCRIPT_PATH/instrument.sh "$MODULE/$TARGET_DIR_NAME" "$MODULE/$OUTPUT_DIR_NAME")
-    # if [[ ! "$INSTRUMENT_OUTPUT" == *"Failed to instrument"* ]];
-    # then 
-    #     LOG_OUTPUT="OK"
-    # else 
-    #     LOG_OUTPUT="NOK"
-    # fi
-    # echo "$(basename "$MODULE") - $LOG_OUTPUT" >> $LOG_FILE
+    echo "Instrumenting module '$(basename "$MODULE")' to $MODULE/$OUTPUT_DIR_NAME"
+    INSTRUMENT_OUTPUT=$($SCRIPT_PATH/instrument.sh "$MODULE/$TARGET_DIR_NAME" "$MODULE/$OUTPUT_DIR_NAME")
+    if [[ ! "$INSTRUMENT_OUTPUT" == *"Failed to instrument"* ]];
+    then 
+        LOG_OUTPUT="OK"
+    else 
+        LOG_OUTPUT="NOK"
+    fi
+    echo "$(basename "$MODULE") - $LOG_OUTPUT" >> $LOG_FILE
 done
